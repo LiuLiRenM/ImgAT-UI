09269c531e3e4ed9492a9a2d1390ad0accdb896b
路绘唯梦 <liuliren.mm@gmail.com>
2024-09-22 21:41:36 +0800

    添加测试模块

---
+++ tests/__init__.py
@@ -0,0 +1,16 @@
+import flet as ft
+
+def main(page: ft.Page):
+    page.floating_action_button = ft.FloatingActionButton(
+        content=ft.Row(
+            [ft.Icon(ft.icons.ADD), ft.Text("Add")], alignment="center", spacing=5
+        ),
+        bgcolor=ft.colors.AMBER_300,
+        shape=ft.RoundedRectangleBorder(radius=5),
+        width=100,
+        mini=True,
+    )
+
+    page.add(ft.Text("Just a text!"))
+
+ft.app(main)
