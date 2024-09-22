7ee8661da91c82ed9b77d3606e07252f6217a4b2
路绘唯梦 <liuliren.mm@gmail.com>
2024-09-22 21:40:12 +0800

    添加常量类

---
+++ src/common/constants.py
@@ -0,0 +1,29 @@
+"""
+常量
+
+"""
+# 中文常量
+APP_NAME = '忆图宝盒'
+WALLPAPER_CENTER = '壁纸中心'
+LOCAL_GALLERY = '本地图库'
+ONLINE_GALLERY = '在线图库'
+DOWNLOAD_CENTER = '下载中心'
+DOWNLOAD_LIST = '下载列表'
+LOCAL_LIST = '本地列表'
+SETTINGS = '设置'
+HOME_PAGE = '首页'
+USERS = '用户'
+LOCAL_GALLERY_CONFIGURATION_TITLE = '本地图库配置'
+GALLERY_PATH_CONFIGURATION_PROMPT = '请先配置图库路径吧~'
+SELECTED_DIR_PROMPT = '已选择的图库路径（可以多选哦~）: '
+CONFIRM_BUTTON = '确定'
+ADD_LOCAL_GALLERY = '添加本地图库'
+CONTENT_OF_THE_PROMPT = '您是否要将已勾选的文件路径作为本地图库的路径吗？'
+YES = '是'
+NO = '否'
+
+# 本地存储key
+GALLERY_DIR_PATH_LIST = 'user.gallery.dir_path_list'
+
+# 图片后缀
+IMAGE_SUFFIX_LIST = ['.png', '.jpg', '.jpeg', '.gif', '.bmp', '.psd', '.webp', '.svg']
